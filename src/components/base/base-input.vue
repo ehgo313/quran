13fd68f16e219609a11c2b5c80222d475643ed6e
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-05-18 15:58:47 +0700

    title size

--- src/components/base/base-input.vue
+++ src/components/base/base-input.vue
@@ -14,6 +14,6 @@ defineProps({
     :type="type"
     :id="id"
     :placeholder="placeholder"
-    class="text-gray-900 border border-gray-200 rounded-lg w-full p-2 placeholder:text-gray-400 focus:outline-none focus:border-sky-600"
+    class="text-gray-900 border border-gray-200 rounded-lg w-full py-2 px-2.5 placeholder:text-gray-400 focus:outline-none focus:border-sky-600"
   />
 </template>
--- src/components/base/base-title.vue
+++ src/components/base/base-title.vue
@@ -1,3 +1,21 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  size: {
+    type: String,
+    default: 'medium',
+  },
+});
+
+const size = computed(() => {
+  return {
+    small: 'text-xl',
+    medium: 'text-2xl',
+  }[props.size];
+});
+</script>
+
 <template>
-  <h1 class="font-bold text-2xl text-gray-900"><slot /></h1>
+  <h1 :class="['font-bold text-gray-900', size]"><slot /></h1>
 </template>
