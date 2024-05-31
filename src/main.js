6e20d243d7fa3d53f9d9526d663273479ca48cb7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-05-31 20:25:34 +0700

    emitter

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import './style.css';
 import App from './App.vue';
 import { useRouter } from './core/router/router';
 import { routes } from './route/routes';
+import { useEmitter } from './core/emitter/emitter';
 
 const app = createApp(App);
 
 useRouter(app, {
   routes,
 });
+useEmitter(app);
 
 app.mount('#app');
