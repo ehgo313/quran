06db3e5cb5b05be338c5a88394f683d3540b0324
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-05-31 18:53:16 +0700

    input state

--- src/components/base/base-input.vue
+++ src/components/base/base-input.vue
@@ -1,11 +1,26 @@
 <script setup>
-defineProps({
+import { computed } from 'vue';
+
+const props = defineProps({
   type: {
     type: String,
     default: 'text',
   },
   id: String,
   placeholder: String,
+  state: {
+    type: String,
+    default: 'default',
+  },
+});
+
+const state = computed(() => {
+  return {
+    default:
+      'border-gray-200 placeholder:text-gray-400 focus:border-sky-600 text-red-900',
+    danger:
+      'border-red-400 placeholder:text-red-400 focus:border-red-600 text-red-600',
+  }[props.state];
 });
 </script>
 
@@ -14,6 +29,6 @@ defineProps({
     :type="type"
     :id="id"
     :placeholder="placeholder"
-    class="text-gray-900 border border-gray-200 rounded-lg w-full py-2 px-2.5 placeholder:text-gray-400 focus:outline-none focus:border-sky-600"
+    :class="['border rounded-lg w-full py-2 px-2.5 focus:outline-none', state]"
   />
 </template>
--- src/features/auth/pages/auth-login-page.vue
+++ src/features/auth/pages/auth-login-page.vue
@@ -3,11 +3,41 @@ import BaseTitle from 'src/components/base/base-title.vue';
 import BaseInput from 'src/components/base/base-input.vue';
 import BaseButton from 'src/components/base/base-button.vue';
 import { useRouter } from 'vue-router';
+import { z } from 'zod';
+import { ref } from 'vue';
 
 const router = useRouter();
 
-function handleSubmit() {
-  router.push({ name: 'dashboard' });
+const errors = ref([]);
+
+const schema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+function hasError(key) {
+  return !!errors.value.find((issue) => issue.path === key);
+}
+
+function getError(key) {
+  return hasError(key)
+    ? errors.value.find((issue) => issue.path === key).message
+    : null;
+}
+
+async function handleSubmit() {
+  const res = await schema.safeParseAsync({});
+
+  if (!res.success) {
+    errors.value = res.error.issues.map((item) => {
+      return {
+        path: item.path[0],
+        message: item.message,
+      };
+    });
+  } else {
+    router.push({ name: 'dashboard' });
+  }
 }
 </script>
 
@@ -16,11 +46,17 @@ function handleSubmit() {
     <div class="w-full max-w-sm space-y-4 px-4 sm:px-0">
       <base-title>Login to Account</base-title>
       <form class="space-y-4" v-on:submit.prevent="handleSubmit">
-        <base-input type="email" id="email" placeholder="Account Email" />
+        <base-input
+          type="email"
+          id="email"
+          placeholder="Account Email"
+          :state="hasError('email') ? 'danger' : 'default'"
+        />
         <base-input
           type="password"
           id="password"
           placeholder="Account Password"
+          :state="hasError('password') ? 'danger' : 'default'"
         />
         <base-button type="submit" fullwidth>Login</base-button>
       </form>
