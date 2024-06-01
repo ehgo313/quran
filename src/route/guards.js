dd035f4ed89f074bc825d00a48d3e52d33a2413c
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-01 11:53:35 +0700

    guest guard

--- src/route/guards.js
+++ src/route/guards.js
@@ -1,3 +1,4 @@
 import { protectAuth } from 'src/features/auth/guards/auth.guard';
+import { protectGuest } from 'src/features/auth/guards/guest.guard';
 
-export const guards = [protectAuth];
+export const guards = [protectAuth, protectGuest];
