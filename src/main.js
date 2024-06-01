fd03310f0e0f6aeea526326baf58a0a721c33b6c
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-01 09:06:38 +0700

    router guards

--- src/main.js
+++ src/main.js
@@ -4,11 +4,15 @@ import App from './App.vue';
 import { useRouter } from './core/router/router';
 import { routes } from './route/routes';
 import { useEmitter } from './core/emitter/emitter';
+import { useStore } from './core/store/store';
+import { guards } from './route/guards';
 
 const app = createApp(App);
 
+useStore(app);
 useRouter(app, {
   routes,
+  guards,
 });
 useEmitter(app);
 
