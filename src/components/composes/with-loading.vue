c28ad8659d2e7b54bf82d39b062844bf2c1cc647
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-12 17:37:21 +0700

    loading compose component

---
+++ src/components/composes/with-loading.vue
@@ -0,0 +1,23 @@
+<script setup>
+import BaseAlert from 'src/components/base/base-alert.vue';
+
+defineProps({
+  loading: Boolean,
+  error: [Boolean],
+  errorMessage: [Error, String],
+});
+</script>
+
+<template>
+  <div>
+    <base-alert v-if="loading" size="sm" color="sky" icon="info"
+      >Loading</base-alert
+    >
+    <template v-else>
+      <base-alert v-if="error" size="sm" color="red" icon="error">{{
+        errorMessage
+      }}</base-alert>
+      <slot v-else />
+    </template>
+  </div>
+</template>
