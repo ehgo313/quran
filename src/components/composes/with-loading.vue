a37f45f32d46bd6bb1daf2c34530a4952e05c1b6
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-13 21:43:54 +0700

    with loading block option

--- src/components/composes/with-loading.vue
+++ src/components/composes/with-loading.vue
@@ -5,19 +5,21 @@ defineProps({
   loading: Boolean,
   error: [Boolean],
   errorMessage: [Error, String],
+  loadingBlock: {
+    type: Boolean,
+    default: true,
+  },
 });
 </script>
 
 <template>
-  <div>
-    <base-alert v-if="loading" size="sm" color="sky" icon="info"
-      >Loading</base-alert
-    >
-    <template v-else>
-      <base-alert v-if="error" size="sm" color="red" icon="error">{{
-        errorMessage
-      }}</base-alert>
-      <slot v-else />
-    </template>
-  </div>
+  <base-alert v-if="loading" size="sm" color="sky" icon="info"
+    >Loading</base-alert
+  >
+  <template v-if="!loading || !loadingBlock">
+    <base-alert v-if="error" size="sm" color="red" icon="error">{{
+      errorMessage
+    }}</base-alert>
+    <slot v-else />
+  </template>
 </template>
