89038260640b1a9530df939049787f3b48153164
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-16 13:16:54 +0700

    base card

---
+++ src/components/base/base-card.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  title: String,
+});
+</script>
+
+<template>
+  <div class="bg-white p-5 rounded-lg shadow space-y-4">
+    <h2 class="font-bold text-xl">{{ title }}</h2>
+    <slot />
+  </div>
+</template>
