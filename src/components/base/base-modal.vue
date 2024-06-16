7a04124140f8325cd6158f2c7832d1fefc5d5605
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-16 13:35:16 +0700

    base modal close slot

--- src/components/base/base-modal.vue
+++ src/components/base/base-modal.vue
@@ -1,5 +1,9 @@
 <script setup>
 const visible = defineModel();
+
+function onClose() {
+  visible.value = false;
+}
 </script>
 
 <template>
@@ -8,7 +12,7 @@ const visible = defineModel();
     class="fixed top-0 left-0 w-full h-full bg-gray-400 bg-opacity-50 p-4 flex justify-center items-center"
   >
     <div class="relative max-w-sm w-full">
-      <slot />
+      <slot :close="onClose" />
     </div>
   </div>
 </template>
