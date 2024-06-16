a45ec4aa80835b1e9b9a1b6054adf04e33d2ab98
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-16 17:18:21 +0700

    base dropdown

---
+++ src/components/base/base-dropdown.vue
@@ -0,0 +1,26 @@
+<script setup>
+defineProps({
+  contentWrapperClass: String,
+});
+
+const visibleDropdown = defineModel();
+
+function onToggleDropdown() {
+  visibleDropdown.value = !visibleDropdown.value;
+}
+function onCloseDropdown() {
+  visibleDropdown.value = false;
+}
+</script>
+
+<template>
+  <div v-click-outside="onCloseDropdown">
+    <slot name="toggle" :toggle="onToggleDropdown" />
+    <div
+      v-show="visibleDropdown"
+      :class="['absolute z-10', contentWrapperClass]"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
