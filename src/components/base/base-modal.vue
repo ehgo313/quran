c181ff78d3a1dabdeb9d92346396a28d5050afa1
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-16 13:38:34 +0700

    modal click outside close

--- src/components/base/base-modal.vue
+++ src/components/base/base-modal.vue
@@ -11,7 +11,7 @@ function onClose() {
     v-if="visible"
     class="fixed top-0 left-0 w-full h-full bg-gray-400 bg-opacity-50 p-4 flex justify-center items-center"
   >
-    <div class="relative max-w-sm w-full">
+    <div class="relative max-w-sm w-full" v-click-outside="onClose">
       <slot :close="onClose" />
     </div>
   </div>
