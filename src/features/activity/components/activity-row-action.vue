1bfd6f2ff8453c6f153d00342ff140366cc11964
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-18 19:31:22 +0700

    activity row action on edit event

--- src/features/activity/components/activity-row-action.vue
+++ src/features/activity/components/activity-row-action.vue
@@ -3,24 +3,32 @@ import { Menu2 as ActionIcon } from '@vicons/tabler';
 import { ref } from 'vue';
 import BaseDropdown from 'src/components/base/base-dropdown.vue';
 
+const emit = defineEmits(['edit']);
+
 const visibleDropdown = ref(false);
+
+function onClickOption(option) {
+  if (option.id === 'edit') {
+    emit('edit');
+  }
+}
 </script>
 
 <template>
   <base-dropdown
     class="flex items-center relative"
     content-wrapper-class="bg-white border -bottom-[70px] right-0 rounded-lg"
+    :options="[
+      { id: 'edit', name: 'Edit' },
+      { id: 'delete', name: 'Delete' },
+    ]"
     v-model="visibleDropdown"
+    @click-option="onClickOption"
   >
     <template #toggle="{ toggle }">
       <button @click="toggle">
         <action-icon class="w-3 h-3" />
       </button>
     </template>
-
-    <div class="flex flex-col py-1">
-      <a href="" class="px-3 py-1 hover:bg-gray-100 text-sm">Edit</a>
-      <a href="" class="px-3 py-1 hover:bg-gray-100 text-sm">Delete</a>
-    </div>
   </base-dropdown>
 </template>
