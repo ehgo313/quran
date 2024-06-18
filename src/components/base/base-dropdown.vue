899e6db9d385431a821aad22b4f6c6e98aa43227
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-18 19:30:47 +0700

    base dropdown on click event

--- src/components/base/base-dropdown.vue
+++ src/components/base/base-dropdown.vue
@@ -1,7 +1,9 @@
 <script setup>
 defineProps({
   contentWrapperClass: String,
+  options: Array,
 });
+const emit = defineEmits(['click-option']);
 
 const visibleDropdown = defineModel();
 
@@ -10,6 +12,9 @@ function onToggleDropdown() {
 }
 function onCloseDropdown() {
   visibleDropdown.value = false;
+}
+function onClickOption(option) {
+  emit('click-option', option);
 }
 </script>
 
@@ -20,7 +25,18 @@ function onCloseDropdown() {
       v-show="visibleDropdown"
       :class="['absolute z-10', contentWrapperClass]"
     >
-      <slot />
+      <slot>
+        <div class="flex flex-col py-1">
+          <a
+            v-for="option in options"
+            :key="option.id"
+            href=""
+            class="px-3 py-1 hover:bg-gray-100 text-sm"
+            @click.prevent="onClickOption(option)"
+            >{{ option.name }}</a
+          >
+        </div>
+      </slot>
     </div>
   </div>
 </template>
