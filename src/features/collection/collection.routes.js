6f26e12f3fed1bbc290b319a2527bb185186a527
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-21 07:11:07 +0700

    collection detail page use layout app

--- src/features/collection/collection.routes.js
+++ src/features/collection/collection.routes.js
@@ -4,6 +4,7 @@ export const collectionRoutes = [
     name: 'collection.detail',
     meta: {
       requireAuth: true,
+      layout: 'LayoutApp',
     },
     component: () => import('./pages/collection-detail-page.vue'),
   },
--- src/features/collection/pages/collection-detail-page.vue
+++ src/features/collection/pages/collection-detail-page.vue
@@ -1,12 +1,9 @@
 <script setup>
-import { Menu2 as ActionIcon } from '@vicons/tabler';
 import BaseTitle from 'src/components/base/base-title.vue';
-import BaseButton from 'src/components/base/base-button.vue';
-import PartialSidebar from 'src/components/partials/partial-sidebar.vue';
 import withLoading from 'src/components/composes/with-loading.vue';
 import { useRequest } from 'src/core/request/request';
 import { useAuthStore } from 'src/features/auth/auth.store';
-import { reactive, ref, computed } from 'vue';
+import { reactive, ref } from 'vue';
 import ActivityEditModal from 'src/features/activity/components/activity-edit-modal.vue';
 import ActivityDeleteConfirm from 'src/features/activity/components/activity-delete-confirm.vue';
 import ActivityList from 'src/features/activity/components/activity-list.vue';
@@ -98,52 +95,38 @@ loadPage();
 </script>
 
 <template>
-  <div
-    class="max-w-2xl mx-auto grid grid-cols-1 sm:grid-cols-4 p-4 sm:py-8 sm:px-0 gap-4 sm:gap-8"
+  <with-loading
+    :loading="loadingCollection"
+    :error="!!errorCollection"
+    :error-message="getErrorCollectionMessage()"
   >
-    <div class="sm:hidden">
-      <base-button color="light" square>
-        <action-icon class="w-4 h-4" />
-      </base-button>
+    <div class="flex items-center justify-between">
+      <base-title size="small">{{ collection.data.name }}</base-title>
+      <a href="" class="text-sky-600" @click.prevent="onCreate">New Activity</a>
     </div>
-    <partial-sidebar />
-    <div class="col-span-3 space-y-2">
-      <with-loading
-        :loading="loadingCollection"
-        :error="!!errorCollection"
-        :error-message="getErrorCollectionMessage()"
-      >
-        <div class="flex items-center justify-between">
-          <base-title size="small">{{ collection.data.name }}</base-title>
-          <a href="" class="text-sky-600" @click.prevent="onCreate"
-            >New Activity</a
-          >
-        </div>
-        <with-loading
-          :loading="loadingActivities"
-          :loading-block="!activitiesLoaded"
-          :error="!!errorActivities"
-          :error-message="getErrorActivitiesMessage()"
-        >
-          <activity-list
-            :activities="activities.data"
-            v-model:creating="creating"
-            @edit="onEdit"
-            @delete="onDelete"
-            @created="onCreated"
-          />
-        </with-loading>
-      </with-loading>
-    </div>
-    <activity-edit-modal
-      :activity="editModal.activity"
-      v-model="editModal.visible"
-      @updated="onUpdated"
-    />
-    <activity-delete-confirm
-      :activity-id="deleteConfirm.activityId"
-      v-model="deleteConfirm.visible"
-      @deleted="onDeleted"
-    />
-  </div>
+    <with-loading
+      :loading="loadingActivities"
+      :loading-block="!activitiesLoaded"
+      :error="!!errorActivities"
+      :error-message="getErrorActivitiesMessage()"
+    >
+      <activity-list
+        :activities="activities.data"
+        v-model:creating="creating"
+        @edit="onEdit"
+        @delete="onDelete"
+        @created="onCreated"
+      />
+    </with-loading>
+  </with-loading>
+  <activity-edit-modal
+    :activity="editModal.activity"
+    v-model="editModal.visible"
+    @updated="onUpdated"
+  />
+  <activity-delete-confirm
+    :activity-id="deleteConfirm.activityId"
+    v-model="deleteConfirm.visible"
+    @deleted="onDeleted"
+  />
 </template>
