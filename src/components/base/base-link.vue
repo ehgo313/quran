81678d82d8f3b2b1df796e35258c0b1ffa3d7950
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-22 10:55:54 +0700

    base link

---
+++ src/components/base/base-link.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  to: [String, Object],
+  href: String,
+  active: Boolean,
+  color: String,
+  native: Boolean,
+});
+
+const base = computed(() => [
+  props.color === 'sky' ? 'text-sky-600' : '',
+  'hover:text-sky-600',
+  props.active ? 'text-sky-600 font-bold' : '',
+]);
+</script>
+
+<template>
+  <a v-if="native" :href="href" :class="base"><slot /></a>
+  <router-link v-else :to="to" :class="base"><slot /></router-link>
+</template>
