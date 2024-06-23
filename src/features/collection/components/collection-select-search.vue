d52eed6318d1cb6fe1cfd1dcb6cddfa03faa0486
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-23 18:34:48 +0700

    collection select search request limit

--- src/features/collection/components/collection-select-search.vue
+++ src/features/collection/components/collection-select-search.vue
@@ -2,6 +2,7 @@
 import BaseSelectSearch from 'src/components/base/base-select-search.vue';
 import { useRequest } from 'src/core/request/request';
 import { useAuthStore } from 'src/features/auth/auth.store';
+import { ref } from 'vue';
 
 const selected = defineModel();
 
@@ -12,20 +13,32 @@ const { data: collections, request } = useRequest('/collections', {
   },
 });
 
+const limit = ref(5);
+
 function loadCollections(params) {
   request({
     params: {
       user_id: authStore.me.userId,
+      limit: limit.value,
       ...params,
     },
   });
 }
 
 function onFocus() {
+  limit.value = 5;
+
   loadCollections({ search: selected.value ? selected.value.name : '' });
 }
 function onSearch(value) {
+  limit.value = 5;
+
   loadCollections({ search: value });
+}
+function onLoadMore() {
+  limit.value += 5;
+
+  loadCollections();
 }
 </script>
 
@@ -36,5 +49,6 @@ function onSearch(value) {
     v-model="selected"
     @focus="onFocus"
     @search="onSearch"
+    @end-scroll="onLoadMore"
   />
 </template>
