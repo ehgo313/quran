bf7024ae9bb2c19c3e1c1926b0b0bbee9a07e3b0
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-25 18:25:58 +0700

    activity index page

---
+++ src/features/activity/pages/activity-index-page.vue
@@ -0,0 +1,11 @@
+<script setup>
+import BaseTitle from 'src/components/base/base-title.vue';
+import ActivityList from 'src/features/activity/components/activity-list.vue';
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <base-title size="small">All Activities</base-title>
+  </div>
+  <activity-list />
+</template>
