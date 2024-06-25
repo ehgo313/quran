a880c0eb3e137149c7b25106c25fd89785d56d45
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-25 18:30:10 +0700

    activity row add to today button

--- src/features/activity/components/activity-row.vue
+++ src/features/activity/components/activity-row.vue
@@ -2,13 +2,18 @@
 import BaseButton from 'src/components/base/base-button.vue';
 import ActivityRowAction from './activity-row-action.vue';
 import { useRequest } from 'src/core/request/request';
+import { ref } from 'vue';
+import { date } from 'src/utils/date';
 
 const props = defineProps({
   activity: Object,
 });
 const emit = defineEmits(['edit', 'delete', 'updated']);
 
-const { loading, request: updateActivity } = useRequest();
+const { request: updateActivity } = useRequest();
+
+const loadingToggleDone = ref(false);
+const loadingAddToToday = ref(false);
 
 function onEdit() {
   emit('edit', props.activity);
@@ -17,6 +22,8 @@ function onDelete() {
   emit('delete', props.activity);
 }
 async function onToggleDone() {
+  loadingToggleDone.value = true;
+
   const [, error] = await updateActivity({
     url: `/activities/${props.activity.id}`,
     method: 'patch',
@@ -28,6 +35,25 @@ async function onToggleDone() {
   if (!error) {
     emit('updated');
   }
+
+  loadingToggleDone.value = false;
+}
+async function onAddToToday() {
+  loadingAddToToday.value = true;
+
+  const [, error] = await updateActivity({
+    url: `/activities/${props.activity.id}`,
+    method: 'patch',
+    data: {
+      date: date(),
+    },
+  });
+
+  if (!error) {
+    emit('updated');
+  }
+
+  loadingAddToToday.value = false;
 }
 </script>
 
@@ -45,9 +71,16 @@ async function onToggleDone() {
       <base-button
         size="extra-small"
         color="light"
-        :loading="loading"
+        :loading="loadingToggleDone"
         @click="onToggleDone(activity)"
         >{{ activity.done ? 'Mark as Todo' : 'Mark as Done' }}</base-button
+      >
+      <base-button
+        size="extra-small"
+        color="light"
+        :loading="loadingAddToToday"
+        @click="onAddToToday(activity)"
+        >Add To Today</base-button
       >
       <activity-row-action
         @edit="onEdit(activity)"
