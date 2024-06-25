857c443c706e2284f2ccce7171a9b5c4aa4958be
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-25 18:26:18 +0700

    activity all route

--- src/features/activity/activity.routes.js
+++ src/features/activity/activity.routes.js
@@ -8,4 +8,13 @@ export const activityRoutes = [
     },
     component: () => import('./pages/activity-today-page.vue'),
   },
+  {
+    path: '/activities',
+    name: 'activity.all',
+    meta: {
+      requireAuth: true,
+      layout: 'LayoutApp',
+    },
+    component: () => import('./pages/activity-index-page.vue'),
+  },
 ];
