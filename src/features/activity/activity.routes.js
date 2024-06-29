9ef4ab7a99275bd0fd276c84d35aa0694a41fa68
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-29 12:24:27 +0700

    activity late page

--- src/features/activity/activity.routes.js
+++ src/features/activity/activity.routes.js
@@ -17,4 +17,13 @@ export const activityRoutes = [
     },
     component: () => import('./pages/activity-index-page.vue'),
   },
+  {
+    path: '/activities/late',
+    name: 'activity.late',
+    meta: {
+      requireAuth: true,
+      layout: 'LayoutApp',
+    },
+    component: () => import('./pages/activity-late-page.vue'),
+  },
 ];
---
+++ src/features/activity/pages/activity-late-page.vue
@@ -0,0 +1,20 @@
+<script setup>
+import BaseTitle from 'src/components/base/base-title.vue';
+import ActivityList from 'src/features/activity/components/activity-list.vue';
+import { date } from 'src/utils/date';
+
+const filter = {
+  start_date: date().subtract(1, 'day').startOf('day').toISOString(),
+  end_date: date().subtract(1, 'day').endOf('day').toISOString(),
+};
+const createPayload = {
+  date: date().toISOString(),
+};
+</script>
+
+<template>
+  <div class="flex items-center justify-between">
+    <base-title size="small">Late Activities</base-title>
+  </div>
+  <activity-list :filter="filter" :create-payload="createPayload" />
+</template>
