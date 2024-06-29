ddccfa8a804de0d4ca06a7140aea7c8640d6a3f7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-06-29 13:03:08 +0700

    late activity filter done false

--- src/features/activity/pages/activity-late-page.vue
+++ src/features/activity/pages/activity-late-page.vue
@@ -6,6 +6,7 @@ import { date } from 'src/utils/date';
 const filter = {
   start_date: date().subtract(1, 'day').startOf('day').toISOString(),
   end_date: date().subtract(1, 'day').endOf('day').toISOString(),
+  done: false,
 };
 const createPayload = {
   date: date().toISOString(),
