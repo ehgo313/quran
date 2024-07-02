119aeb3edfc57d03b351383dcb73f03c742198e7
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-02 18:30:13 +0700

    load me on init app

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <script setup>
 import PartialToast from 'src/components/partials/partial-toast.vue';
-import { useRoute } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
+import { useAuthStore } from 'src/features/auth/auth.store';
 
 const route = useRoute();
+const router = useRouter();
+const authStore = useAuthStore();
+
+async function loadMe() {
+  if (authStore.isLoggedIn) {
+    const [, error] = await authStore.loadMe();
+
+    if (error) {
+      authStore.logout();
+      router.push({ name: 'auth.login' });
+    }
+  }
+}
+
+loadMe();
 </script>
 
 <template>
