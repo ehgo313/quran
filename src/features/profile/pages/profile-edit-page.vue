89a7485904f89cf1ca65419d4b1645077198a070
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-04 19:29:58 +0700

    validate form edit profile

--- src/features/profile/pages/profile-edit-page.vue
+++ src/features/profile/pages/profile-edit-page.vue
@@ -6,8 +6,11 @@ import BaseButton from 'src/components/base/base-button.vue';
 import { useAuthStore } from 'src/features/auth/auth.store';
 import { reactive, ref } from 'vue';
 import ProfileEditPasswordModal from 'src/features/profile/components/profile-edit-password-modal.vue';
+import { z } from 'zod';
+import { useValidation } from 'src/core/validation/validation';
 
 const authStore = useAuthStore();
+const { hasError, getError, validate, resetError } = useValidation();
 
 const form = reactive({
   email: authStore.me.email,
@@ -15,25 +18,58 @@ const form = reactive({
 });
 const editPasswordModalVisible = ref(false);
 
+const schema = z.object({
+  email: z
+    .string({
+      required_error: 'email is required',
+      invalid_type_error: 'email must be a string',
+    })
+    .email({ message: 'email must be a valid string' }),
+  name: z
+    .string({
+      required_error: 'name is required',
+      invalid_type_error: 'name must be a string',
+    })
+    .min(1, { message: 'name cannot be empty' }),
+});
+
 function onEditPassword() {
   editPasswordModalVisible.value = true;
+}
+async function onSave() {
+  resetError();
+
+  const [data, errorValidate] = await validate(schema, form);
 }
 </script>
 
 <template>
   <base-title size="small">Profile</base-title>
-  <base-form-item label="Email">
-    <base-input type="email" placeholder="Email" v-model="form.email" />
-  </base-form-item>
-  <base-form-item label="Name">
-    <base-input placeholder="Name" v-model="form.name" />
-  </base-form-item>
-  <div class="flex gap-x-2">
-    <base-button>Simpan</base-button>
-    <base-button color="light" @click="onEditPassword"
-      >Edit Password</base-button
-    >
-  </div>
+  <form class="space-y-2" @submit.prevent="onSave">
+    <base-form-item label="Email">
+      <base-input
+        type="email"
+        placeholder="Email"
+        v-model="form.email"
+        :state="hasError('email') ? 'danger' : 'default'"
+        :message="getError('email')"
+      />
+    </base-form-item>
+    <base-form-item label="Name">
+      <base-input
+        placeholder="Name"
+        v-model="form.name"
+        :state="hasError('name') ? 'danger' : 'default'"
+        :message="getError('name')"
+      />
+    </base-form-item>
+    <div class="flex gap-x-2">
+      <base-button type="submit">Simpan</base-button>
+      <base-button color="light" @click="onEditPassword"
+        >Edit Password</base-button
+      >
+    </div>
+  </form>
   <div>
     <profile-edit-password-modal v-model="editPasswordModalVisible" />
   </div>
