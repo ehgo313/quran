ec9c361bfff54be97a5dea886ad6b0e391e11e96
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-07-04 19:13:50 +0700

    profile edit page

---
+++ src/features/profile/pages/profile-edit-page.vue
@@ -0,0 +1,17 @@
+<script setup>
+import BaseFormItem from 'src/components/base/base-form-item.vue';
+import BaseInput from 'src/components/base/base-input.vue';
+import BaseTitle from 'src/components/base/base-title.vue';
+import BaseButton from 'src/components/base/base-button.vue';
+</script>
+
+<template>
+  <base-title size="small">Profile</base-title>
+  <base-form-item label="Email">
+    <base-input type="email" placeholder="Email" />
+  </base-form-item>
+  <base-form-item label="Name">
+    <base-input placeholder="Name" />
+  </base-form-item>
+  <base-button>Simpan</base-button>
+</template>
