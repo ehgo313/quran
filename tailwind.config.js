ef5f6fe1d8b240266aa8cd6504630c90e670ef67
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2024-09-20 19:00:10 +0700

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'sans': ['Rubik', ...defaultTheme.fontFamily.sans],
+        'sans': ['LPMQ', ...defaultTheme.fontFamily.sans],
       },
     }
   },
